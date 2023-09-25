574433c9c166c829566564dbe7e5fd78b7b53d23
Gabriel Andrade <42943566+gandrade4@users.noreply.github.com>
2023-09-25 19:58:02 -0300

    js still need some changes isnt working

--- Aula 13 - Revisão/app.js
+++ Aula 13 - Revisão/app.js
@@ -2,10 +2,11 @@ const form = document.getElementById("form")
 const categorySelect = document.getElementById("category")
 const descriptionInput = document.getElementById("description")
 const dateInput = document.getElementById("date")
+const pendingUl = document.getElementById("pending")
 
 form.addEventListener("submit", event => {
     event.preventDefault()
-    const category = categorySelect.getAttribute('value')
+    const category = categorySelect.value
     const description = descriptionInput.value 
     const date = dateInput.value 
     createTask(category, description, date)
@@ -14,4 +15,17 @@ form.addEventListener("submit", event => {
 
 function createTask(category, description, date){
     console.log('${category} ${description} ${date}')
+
+    const taskLi = document.createElement{"li"}
+    const label = document.createElement{"label"}
+    const checkbox = document.createElement{"input"}
+
+
+    checkbox.type = "checkbox"
+    checkbox.addEventListener{'change', finish}
+    
+    label.textContent = description
+    taskLi.appendChild(label)
+    
+    pendingUl.appendChild(taskLi)
 }
