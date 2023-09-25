93e15f6ba90cac822bb6e3b6d85fadd81ed9425f
Gabriel Andrade <42943566+gandrade4@users.noreply.github.com>
2023-09-25 19:37:00 -0300

    receiving the form input

--- Aula 13 - Revisão/app.js
+++ Aula 13 - Revisão/app.js
@@ -1,4 +1,17 @@
 const form = document.getElementById("form")
+const categorySelect = document.getElementById("category")
+const descriptionInput = document.getElementById("description")
+const dateInput = document.getElementById("date")
+
 form.addEventListener("submit", event => {
     event.preventDefault()
+    const category = categorySelect.getAttribute('value')
+    const description = descriptionInput.value 
+    const date = dateInput.value 
+    createTask(category, description, date)
 })
+
+
+function createTask(category, description, date){
+    console.log('${category} ${description} ${date}')
+}
