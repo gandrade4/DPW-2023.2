98c8752ed0208865a7bcdcf159325b8d0d333061
Gabriel Andrade <42943566+gandrade4@users.noreply.github.com>
2023-09-26 18:59:04 -0300

    update

--- Aula 13 - Revisão/app.js
+++ Aula 13 - Revisão/app.js
@@ -3,6 +3,7 @@ const categorySelect = document.getElementById("category")
 const descriptionInput = document.getElementById("description")
 const dateInput = document.getElementById("date")
 const pendingUl = document.getElementById("pending")
+const finishedUl = document.getElementById("finished")
 
 form.addEventListener("submit", event => {
     event.preventDefault()
@@ -12,20 +13,33 @@ form.addEventListener("submit", event => {
     createTask(category, description, date)
 })
 
-
 function createTask(category, description, date){
     console.log('${category} ${description} ${date}')
 
-    const taskLi = document.createElement{"li"}
-    const label = document.createElement{"label"}
-    const checkbox = document.createElement{"input"}
+    const taskLi = document.createElement("li")
+    const label = document.createElement("label")
+    const checkbox = document.createElement("input")
 
 
     checkbox.type = "checkbox"
-    checkbox.addEventListener{'change', finish}
+    checkbox.addEventListener('change', finish)
+    taskLi.appendChild(checkbox)
     
     label.textContent = description
     taskLi.appendChild(label)
+    taskLi.classList.add(category)
+
     
     pendingUl.appendChild(taskLi)
+}
+
+function finish(event){
+    const parent = event.target.parentNode
+    if(event.target.checked){
+        pendingUl.removeChild(parent)
+        finishedUl.appendChild(parent)
+    }else{
+        finishedUl.removeChild(parent)
+        pendingUl.appendChild(parent)
+    }
 }
